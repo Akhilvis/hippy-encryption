c9102de3e2bcd10933ae05b13c15ba802d2af5ab
akhil <akhilviswam000@gmail.com>
2020-04-23 23:37:58 +0530

    class defined

---
+++ main.py
@@ -0,0 +1,11 @@
+class HippyEncryption:
+    def __init__(self):
+        encryption_tree = {1: "s", 2: "ox", 3: "hen", 4: "duck", 5: "chick", 6: "buffalo"}
+        sample_plaintext = "haitim"
+        sample_key = "jdilsm"
+        division_factor = len(encryption_tree)
+
+    def encrypt_plain_text(self):
+        pass
+
+
